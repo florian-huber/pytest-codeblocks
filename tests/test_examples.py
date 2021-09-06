2fdf62a5ad45abe4d0b23312cd978ff790e444c5
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2021-09-06 20:09:27 +0200

    Update test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,4 +12,4 @@ def test_unicode_markdown(testdir):
     this_dir = pathlib.Path(__file__).resolve().parent
     testdir.copy_example(this_dir / "examples_unicode.md")
     result = testdir.runpytest("--codeblocks")
-    result.assert_outcomes(passed=5)
+    result.assert_outcomes(passed=2)
