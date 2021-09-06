f2f0ccaa3c83304ccf9d9e8fff8101dc0831ea5f
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2021-09-06 20:00:55 +0200

    Update test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,4 +5,4 @@ def test_markdowns_in_test_folder(testdir):
     this_dir = pathlib.Path(__file__).resolve().parent
     testdir.copy_example(this_dir / "example.md")
     result = testdir.runpytest("--codeblocks")
-    result.assert_outcomes(passed=1)
+    result.assert_outcomes(passed=7)
