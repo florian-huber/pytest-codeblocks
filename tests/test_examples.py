101200c8687ffcbedec69a408693876a6df8fb2c
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2021-09-06 20:05:15 +0200

    Update test_examples.py

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,15 @@
 import pathlib
 
 
-def test_markdowns_in_test_folder(testdir):
+def test_example_markdown(testdir):
     this_dir = pathlib.Path(__file__).resolve().parent
     testdir.copy_example(this_dir / "example.md")
     result = testdir.runpytest("--codeblocks")
     result.assert_outcomes(passed=7)
+
+
+def test_unicode_markdown(testdir):
+    this_dir = pathlib.Path(__file__).resolve().parent
+    testdir.copy_example(this_dir / "examples_unicode.md")
+    result = testdir.runpytest("--codeblocks")
+    result.assert_outcomes(passed=5)
